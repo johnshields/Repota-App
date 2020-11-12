4545343f6f7a79da04ccd77edf3241289d94636f
johnshields <G00348436@gmit.ie>
2020-11-12 20:14:14 +0000

    golang back server & mysql

--- Repota/BackEnd/server.go
+++ Repota/BackEnd/server.go
@@ -1,80 +1,62 @@
+// golang server
+// mysql database
+// https://golang.org/doc/articles/wiki/
+// https://godoc.org/github.com/go-sql-driver/mysql
+// https://www.golangprograms.com/example-of-golang-crud-using-mysql-from-scratch.html
+// https://golangbot.com/connect-create-db-mysql/
+// https://medium.com/@hugo.bjarred/mysql-and-golang-ea0d620574d2
+
 package main
 
 import (
 	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/gorilla/mux"
 	"log"
 	"net/http"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
-type Autos struct {
-	Id    int
-	brand string
-	model string
-}
-
-func conDB() (db *sql.DB) {
-	dbDriver := "mysql"
-	dbUser := "root"
-	dbPass := ""
-	dbName := "repotadb"
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
-	if err != nil {
-		panic(err.Error())
-	}
-	return db
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	db := conDB()
-	selDB, err := db.Query("show tables")
-	if err != nil {
-		panic(err.Error())
-	}
-	auto := Autos{}
-	res := []Autos{}
-	for selDB.Next() {
-		var id int
-		var brand, model string
-		err = selDB.Scan(&id, &brand, &model)
-		if err != nil {
-			panic(err.Error())
-		}
-		auto.Id = id
-		auto.brand = brand
-		auto.model = model
-		res = append(res, auto)
-	}
-	defer db.Close()
-}
-
-func Show(w http.ResponseWriter, r *http.Request) {
-	db := conDB()
-	nId := r.URL.Query().Get("id")
-	selDB, err := db.Query("SELECT * FROM automobiles", nId)
-	if err != nil {
-		panic(err.Error())
-	}
-	auto := Autos{}
-	for selDB.Next() {
-		var id int
-		var name, city string
-		err = selDB.Scan(&id, &name, &city)
-		if err != nil {
-			panic(err.Error())
-		}
-		auto.Id = id
-		auto.brand = name
-		auto.model = city
-	}
-
-	defer db.Close()
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Repota %s!", r.URL.Path[1:])
 }
 
 func main() {
+	//launch server
+	http.HandleFunc("/", handler)
 	log.Println("Server started on: http://localhost:8080")
-	http.HandleFunc("/", Index)
-	http.HandleFunc("/show", Show)
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	connDB()
+}
+
+func connDB() {
+	// connect to mysql
+	fmt.Println("Go connect to MySQL")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/repotadb")
+
+	if err != nil {
+		log.Fatal("Unable to connect to repotadb")
+	}
+
+	fmt.Println("Connected to repotadb")
+	defer db.Close()
+
+	results, err := db.Query("select * from automobiles")
+	if err != nil {
+		log.Fatal("Unable to fetch table rows")
+	}
+	for results.Next() {
+		var (
+			id    int
+			brand string
+			model string
+		)
+		err = results.Scan(&id, &brand, &model)
+		if err != nil {
+			log.Fatal("Unable to parse row")
+		}
+		fmt.Printf("\n ID: %d, Brand: %s, Model: %d \n", id, brand, model)
+
+	}
 }
